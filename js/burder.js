33e1e54a7f9fc710b5253bd33ce1832a6d7adc91
Nikita <nikki-xit@mail.ru>
2022-01-10 10:37:40 +0300

    git_Bobr

---
+++ js/burder.js
@@ -0,0 +1,3 @@
+const burger=document.getElementById('burger');const page=document.getElementById('page');const body=document.body;const element=document.querySelectorAll('.modileMenu--item');burger.addEventListener('click',(event)=>{if(body.classList.contains('show-sidebar')){closeSidebar()}else{showSidebar()}});function showSidebar(){const mask=document.createElement('div');mask.classList.add('page__mask');mask.addEventListener('click',closeSidebar);body.appendChild(mask);body.classList.add('show-sidebar')}
+function closeSidebar(){body.classList.remove('show-sidebar');document.querySelector('.page__mask').remove()}
+for(let elem of element){elem.addEventListener('click',closeSidebar)}
